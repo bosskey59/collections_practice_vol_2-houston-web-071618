af7af89a50f0e4501e6fed61d6f5a6521a34ef76
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-28 20:56:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
 # your code goes here
+def begins_with_r(array)
+  array.all? { |x| x.start_with?("r") }
+end
+
+def contain_a(array)
+  array.select{ |x| x.include?("a")}
+  
+end
+
+def first_wa(array)
+  array.find { |x| x.match(/^wa/) }
+  
+end
+
+def remove_non_strings(array)
+  array.delete_if { |x| !(x.is_a? String) }
+  
+end
+
+def count_elements(array)
+  
+end
+
+def merge_data
+  
+end
+
+def find_cool
+  
+end
+
+def organuze_schools
+  
+end
